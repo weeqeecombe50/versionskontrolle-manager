1c5232d524fba64033271ecdb9578a711e19cf5e
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-25 17:47:46 +0300

    Refactor welcome message to enrich user experience.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Version Control Manager!');
+  res.send('Welcome to the Version Control Manager! Navigate through our features easily.');
 });
 
 app.listen(PORT, () => {
